4bdbf9836cd0be05050891d9111ae1a56f386319
Parth Nilkanth <nilkanthparth@gmail.com>
2024-04-01 20:17:39 +0530

    Exceptions added

--- app.py
+++ app.py
@@ -56,51 +56,57 @@ def solapur():
 def crop():
     return render_template('crop.html')
 
+@app.route('/error')
+def error():
+    return render_template('Error.html')
+
 @app.route('/prediction', methods=['POST','GET'])
 def prediction():
-    data = pd.read_csv('Crop and fertilizer dataset.csv')
+    try:
+        data = pd.read_csv('Crop and fertilizer dataset.csv')
 
     # Retrieve the form data
-    district = request.form['District_Name']
-    soil_color = request.form['soil_color']
-    nitrogen = request.form['Nitrogen']
-    phosphorus = request.form['Phosphorus']
-    potassium = request.form['Potassium']
-    pH = request.form['pH']
-    rainfall = request.form['Rainfall']
-    temperature = request.form['Temperature']
+        district = request.form['District_Name']
+        soil_color = request.form['soil_color']
+        nitrogen = request.form['Nitrogen']
+        phosphorus = request.form['Phosphorus']
+        potassium = request.form['Potassium']
+        pH = request.form['pH']
+        rainfall = request.form['Rainfall']
+        temperature = request.form['Temperature']
 
     # Create a dataframe from the form data
-    input_categorical = pd.DataFrame({
-        'District_Name': [district],
-        'Soil_color': [soil_color],
-    })
-    input_numerical = pd.DataFrame({
-        'Nitrogen': [nitrogen],
-        'Phosphorus': [phosphorus],
-        'Potassium': [potassium],
-        'pH': [pH],
-        'Rainfall': [rainfall],
-        'Temperature': [temperature]
-    })
+        input_categorical = pd.DataFrame({
+            'District_Name': [district],
+            'Soil_color': [soil_color],
+        })
+        input_numerical = pd.DataFrame({
+            'Nitrogen': [nitrogen],
+            'Phosphorus': [phosphorus],
+            'Potassium': [potassium],
+            'pH': [pH],
+            'Rainfall': [rainfall],
+            'Temperature': [temperature]
+        })
 
     # Perform the necessary data encoding for the input data (similar to the code you provided)
-    X_categorical = data[['District_Name', 'Soil_color']]
-    X_numerical = data[['Nitrogen', 'Phosphorus', 'Potassium', 'pH', 'Rainfall', 'Temperature']]
-    categorical_columns = ['District_Name', 'Soil_color']
-    X_categorical_encoded, encoder = perform_encoding(X_categorical, categorical_columns)
+        X_categorical = data[['District_Name', 'Soil_color']]
+        X_numerical = data[['Nitrogen', 'Phosphorus', 'Potassium', 'pH', 'Rainfall', 'Temperature']]
+        categorical_columns = ['District_Name', 'Soil_color']
+        X_categorical_encoded, encoder = perform_encoding(X_categorical, categorical_columns)
 
     # encoder=OneHotEncoder(handle_unknown='ignore', sparse=False, sparse_output=False)
-    input_categorical_encoded, _ = perform_encoding(input_categorical, categorical_columns, encoder)
+        input_categorical_encoded, _ = perform_encoding(input_categorical, categorical_columns, encoder)
 
-    X_encoded = pd.concat([X_categorical_encoded, X_numerical], axis=1)
-    input_encoded = pd.concat([input_categorical_encoded, input_numerical], axis=1)
+        X_encoded = pd.concat([X_categorical_encoded, X_numerical], axis=1)
+        input_encoded = pd.concat([input_categorical_encoded, input_numerical], axis=1)
 
     # Make predictions using the loaded model
-    prediction = model.predict(input_encoded)
-    link = data[(data['Crop'] == prediction[0][0]) & (data['Fertilizer'] == prediction[0][1])]['Link'].values[0]
-    return render_template('result.html',prediction1=prediction[0][0],prediction2=prediction[0][1],plink=link)
-
+        prediction = model.predict(input_encoded)
+        link = data[(data['Crop'] == prediction[0][0]) & (data['Fertilizer'] == prediction[0][1])]['Link'].values[0]
+        return render_template('result.html',prediction1=prediction[0][0],prediction2=prediction[0][1],plink=link)
+    except IndexError as e:
+        return render_template('Error.html')
 # AgroExpert Code End
 
 db = SQLAlchemy(app)
